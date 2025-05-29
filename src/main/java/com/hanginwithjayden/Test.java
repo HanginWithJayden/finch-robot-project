9b0c8774d2bbb0f021b44dc59cfb26a8bfc0bdf1
Jayden Haynes <jhaynes0018@mymail.lausd.net>
2025-05-29 13:22:00 -0700

    commit init

---
+++ src/main/java/com/hanginwithjayden/Test.java
@@ -0,0 +1,14 @@
+package com.hanginwithjayden;
+
+import com.birdbrain.Finch;
+
+public class Test {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+
+
+        bird.stop();
+        bird.disconnect();
+    }
+}
