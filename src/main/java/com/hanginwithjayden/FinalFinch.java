ba4050be3ee7fd746976f3189da39806032b8d39
Jayden Haynes <jhaynes0018@mymail.lausd.net>
2025-06-03 13:09:46 -0700

    Finished code successfully

--- src/main/java/com/hanginwithjayden/FinalFinch.java
+++ src/main/java/com/hanginwithjayden/FinalFinch.java
@@ -8,21 +8,30 @@ public class FinalFinch {
 
         // Code to make finch move forward and stop before it hits a wall
         System.out.println(bird.getDistance());
-        bird.setMove("F", 112, 100);
+        bird.setMove("F", 110, 100);
         // Make the robot turn left
-        bird.setTurn("L", 85, 100);
+        bird.setTurn("L", 75, 100);
         // Does all again
         System.out.println(bird.getDistance());
-        bird.setMove("F", bird.getDistance() - 0.50, 75);
-        bird.setTurn("L", 90, 100);
-        // Continue the path
+        bird.setMove("F", bird.getDistance() - 2, 75);
+        bird.setTurn("L", 75, 100);
+        // Finish the path
         bird.setMove("F", 30, 100);
-        bird.setTurn("R", 100, 100);
-        System.out.println(bird.getDistance());
-        bird.setMove("F", 60, 75);
         bird.setTurn("R", 90, 100);
         System.out.println(bird.getDistance());
-        bird.setMove("F", 125, 75);
+        bird.setMove("F", 45, 75);
+        bird.setTurn("R", 90, 100);
+        System.out.println(bird.getDistance());
+        bird.setMove("F", 115, 75);
+        bird.setTurn("R", 100, 100);
+        System.out.println(bird.getDistance());
+        bird.setMove("F", bird.getDistance() - 4, 75);
+        bird.setTurn("L", 90, 100);
+        System.out.println(bird.getDistance());
+        bird.setMove("F", bird.getDistance() - 4, 75);
+        bird.setTurn("L", 100, 100);
+        System.out.println(bird.getDistance());
+        bird.setMove("F", bird.getDistance() - 2, 75);
 
         bird.stop();
         bird.disconnect();
